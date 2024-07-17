c0bf9572bf72b693ef7a0a5ab073a803d64025f7
Mel Andrew Getutua <melandrewgetutua@gmail.com>
2024-07-17 19:02:50 +0800

    added link for seeding

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,6 +3,7 @@
 import { FaceFrownIcon } from '@heroicons/react/16/solid';
 import Header from '@/app/ui/Header/Header';
 import { Button } from '@nextui-org/react';
+import Link from 'next/link';
 
 const Error = ({
   error,
@@ -16,7 +17,14 @@ const Error = ({
       <Header />
       <div className='max-w-[400px] mx-auto text-center'>
         <FaceFrownIcon />
-        <h2 className='text-3xl font-bold'>Something went wrong</h2>
+        <h2 className='text-3xl font-bold'>
+          Something went wrong. Make sure you have seeded the database first.
+          You can seed using{' '}
+          <Link href={`http://localhost:3001/seed`} className='text-sky-300'>
+            this
+          </Link>{' '}
+          link:{' '}
+        </h2>
         <Button onClick={() => reset()} className='mt-4 font-bold text-lg'>
           Try again
         </Button>
